39879a2af491d0284e0b0aa26f6dcfe0e002636a
yashgarg19 <gargyash9057@gmail.com>
2025-01-23 22:44:37 +0530

    login form field creation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'signup',
+    component : SignUpComponent,
+    pathMatch:'full',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
